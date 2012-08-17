44bb4e997d8753b2c0636df95f26b57e4fe40d91
Martin Klepsch <martinklepsch@gmail.com>
2012-08-17 16:43:33 +0200

    adds validation error handling

--- app.rb
+++ app.rb
@@ -11,8 +11,15 @@ class Configure < Sinatra::Base
   end
 
   post '/setup_username' do
-    github_uri = URI(params[:github_clone_url])
-    haml :copy_field, :locals => {:username => github_uri.path}, :layout => (request.xhr? ? false : :layout)
+    clone_url = params[:github_clone_url]
+    if clone_url.start_with?("git@github.com:") and clone_url.end_with?(".git")
+      github_uri = clone_url.split(":").last
+      username = github_uri.split("/").first
+      repo = github_uri.split("/").last
+      haml :copy_field, :locals => {:username => username}, :layout => (request.xhr? ? false : :layout)
+    else
+      haml :not_private_clone_url, :layout => (request.xhr? ? false : :layout)
+    end
   end
 
   get '/:id' do
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -4,7 +4,7 @@ $('#github_info').submit(function() { // catch the form's submit event
         type: $(this).attr('method'), // GET or POST
         url: $(this).attr('action'), // the file to call
         success: function(response) { // on success..
-            $('#copyfield').append(response); // update the DIV
+            $('#copyfield').html(response); // update the DIV
         }
     });
     $('#copyfield').show()

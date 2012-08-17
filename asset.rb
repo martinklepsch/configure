7d5e1e6bb3fea73277c20e5b6c75fff98b5c2fc8
Martin Klepsch <martinklepsch@gmail.com>
2012-08-17 13:31:48 +0200

    adds asset handling and views

--- asset.rb
+++ asset.rb
@@ -1,9 +1,3 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'bourbon'
-require 'sprockets'
-require './app.rb'
-
 map '/assets' do
   environment = Sprockets::Environment.new
   environment.append_path 'assets/javascripts'
@@ -11,8 +5,4 @@ map '/assets' do
   environment.append_path 'assets/images'
   #environment.append_path 'vendor/assets/jquery'
   run environment
-end
-
-map '/' do
-  run Configure
 end
---
+++ assets/javascripts/app.js
@@ -0,0 +1,12 @@
+$('#github_info').submit(function() { // catch the form's submit event
+    $.ajax({ // create an AJAX call...
+        data: $(this).serialize(), // get the form data
+        type: $(this).attr('method'), // GET or POST
+        url: $(this).attr('action'), // the file to call
+        success: function(response) { // on success..
+            $('#copyfield').append(response); // update the DIV
+        }
+    });
+    $('#copyfield').show()
+    return false; // cancel original event to prevent form submitting
+});

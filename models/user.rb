f7b0dad4b4db6da1abbbe93821b845fe7d4950af
Martin Klepsch <martinklepsch@gmail.com>
2012-08-18 00:15:59 +0200

    adds user model

---
+++ models/user.rb
@@ -0,0 +1,7 @@
+require 'datamapper'
+
+class User
+  include DataMapper::Resource
+  property :name, String, :unique => true, :key => true, :required => true
+  property :clone_url, String, :required => true
+end

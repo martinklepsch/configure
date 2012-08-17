bcc3db0da57794ac0f5f312ef42f0c459bd60f3e
Martin Klepsch <martinklepsch@gmail.com>
2012-08-17 13:28:32 +0200

    adds all needed routes and handlers for ajax stuff

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'rubygems'
 require 'sinatra'
-# require 'haml'
-# require 'uri'
+require 'haml'
+require 'uri'
 # require 'mongo'
 
-get '/' do
-  redirect '/pinit.sh'
-end
 
-get '/pinit.sh' do
-  send_file 'pinit.sh'
+class Configure < Sinatra::Base
+  get '/' do
+    haml :index
+  end
+
+  post '/setup_username' do
+    github_uri = URI(params[:github_clone_url])
+    haml :copy_field, :locals => {:username => github_uri.path}, :layout => (request.xhr? ? false : :layout)
+  end
+
+  get '/:id' do
+    send_file 'pinit.sh'
+  end
 end

84c649ff520e92ad568fbae2156768659241522c
Martin Klepsch <martinklepsch@gmail.com>
2012-08-24 10:27:57 +0200

    mixpanel tracking

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,3 +1,4 @@
+mixpanel.track('page viewed', {'page name' : document.title, 'url' : window.location.pathname});
 $('#github_info').submit(function() { // catch the form's submit event
     $.ajax({ // create an AJAX call...
         data: $(this).serialize(), // get the form data
@@ -7,6 +8,7 @@ $('#github_info').submit(function() { // catch the form's submit event
             $('#copyfield').html(response); // update the DIV
         }
     });
+    mixpanel.track("clicked Show me more");
     $('#copyfield').show()
     return false; // cancel original event to prevent form submitting
 });
